ed70e3602c504fb73270b1aee59e9ed0449741b3
noodaj <jadon.loi.68@gmail.com>
2021-12-17 16:09:42 -0800

    game logic

--- game.js
+++ game.js
@@ -16,33 +16,102 @@ function computerPlay() {
             choice = "paper";
             break;
     }
-    console.log("Generated " + choice);
+
     return choice;
 }
 
 function playRound(playerChoice, computerChoice) {
     let winner = "";
     if (playerChoice == computerChoice) {
-        console.log("Tie");
+        winner = "Tie"
     }
     if ((playerChoice == "rock" && computerChoice == "scissor")
         || (playerChoice == "paper" && computerChoice == "rock")
         || (playerChoice == "scissor" && computerChoice == "paper")) {
         winner = "Player";
         playerScore++;
-        console.log("Player wins with score of " + playerScore);
     }
     else if ((computerChoice == "rock" && playerChoice == "scissor")
         || (computerChoice == "paper" && playerChoice == "rock")
         || (computerChoice == "scissor" && playerChoice == "paper")) {
         winner = "Computer";
         compScore++;
-        console.log("Computer wins with score of " + compScore);
     }
 
     return winner;
 }
 
+// adds event listeners for buttons clicked 
+rockbtn.addEventListener('click', () => click('rock'))
+scissorbtn.addEventListener('click', () => click('scissor'))
+paperbtn.addEventListener('click', () => click('paper'))
+
+
+//elements by id
+const player = document.getElementById('user')
+const compPlayer = document.getElementById('comp')
+const pScore = document.getElementById('playerScore')
+const cScore = document.getElementById('compScore')
+const rock = document.getElementById('rockbtn')
+const scissor = document.getElementById('scissorbtn')
+const paper = document.getElementById('paper')
+const headerScore = document.getElementsByClassName('scoreInfo')
+const headerMsg = document.getElementsByClassName('scoreMessage')
+const img = document.querySelector('playerimg')
+
+function gameOver() {
+    return (playerScore === 5) || (compScore === 5)
+}
+
+function click(playerChoice) {
+
+
+    let gen = computerPlay()
+}
+
+function updateChoice(playerChoice, computerChoice) {
+    switch (playerChoice) {
+        case 'rock':
+            img.src = "pics/rock.png";
+            break
+        case 'paper':
+            img.src = "pics/paper.png"
+            break
+        case 'scissor':
+            img.src = "pics/scissor.png"
+            break
+
+    }
+
+    switch (computerChoice) {
+        case 'rock':
+            img.src = "pics/rock.png";
+            break
+        case 'paper':
+            img.src = "pics/paper.png"
+            break
+        case 'scissor':
+            img.src = "pics/scissor.png"
+            break
+
+    }
+}
+
+function updateScore(winner){
+    switch(winner){
+        case 'Player':
+            headerScore.textContent = "You won!"
+            break
+        case 'Computer':
+            headerScore.textContent = "You lost!"
+            break
+        default:
+            headerScore.textContent = "It's a tie!"
+            break
+    }
+
+
+}
 function game() {
     for (i = 0; i < 5; i++) {
         let input = prompt("What is your selection?");
@@ -52,10 +121,10 @@ function game() {
     if (playerScore > compScore) {
         console.log("You won!");
     }
-    else if(compScore > playerScore){
+    else if (compScore > playerScore) {
         console.log("Computer won!");
     }
-    else{
+    else {
         console.log("Tie!");
     }
 }
